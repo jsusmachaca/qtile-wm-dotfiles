150d740393b1a27de347cfcb635ef9da08a4086f
jsusmachaca <falcorgd@gmail.com>
2024-07-28 16:27:47 -0500

    update cli
    Se agregó nueva funcionalidad a la cli de qtheme, como la posibilidad de poder elegir la posición de la barra, el thema de la terminal de kitty. Se añadió la posibilidad de introducir flags.

---
+++ qtile/bin/colors.py
@@ -0,0 +1,19 @@
+
+def red (text):
+    print(f'\033[31m{text}\033[00m')
+
+def yelow (text):
+    print(f'\033[33m{text}\033[00m')
+
+def green (text):
+    print(f'\033[32m{text}\033[00m')
+
+def blue (text):
+    print(f'\033[36m{text}\033[00m')
+
+def magenta (text):
+    print(f'\033[35m{text}\033[00m')
+
+def imagenta (text):
+    return input(f'\033[35m{text}\033[00m').strip()
+    
